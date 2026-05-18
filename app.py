655da985a70d64ffdaaef5b9dbafc00b33f4c146
Leh001 <leh001@edu.zealand.dk>
2026-05-18 11:47:08 +0200

    succesful motion sensor test

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from gpiozero import Buzzer
+from signal import pause
+from gpiozero import Buzzer, MotionSensor
 from time import sleep
+
+
+# Buzzer
 
 buzzer = Buzzer(27)  # GPIO27, pin 13.
 
 # 2 Beeps and then turn off.
-buzzer.on()
-sleep(1)
-buzzer.off()
-sleep(1)
-buzzer.on()
-sleep(1)
-buzzer.off()
+#buzzer.on()
+#sleep(1)
+#buzzer.off()
+#sleep(1)
+#buzzer.on()
+#sleep(1)
+#buzzer.off()
+
+# Motion sensor
+sensor = MotionSensor(22)  # GPIO22, pin 15
+
+def motion_detected():
+    print("Motion")
+
+def motion_stopped():
+    print("No motion")
+
+sensor.when_motion = motion_detected
+sensor.when_no_motion = motion_stopped
+
+pause()
