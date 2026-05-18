d68f6c6effc5afcd3ed6999b68ba1a1bb7b33465
Leh001 <leh001@edu.zealand.dk>
2026-05-18 11:18:37 +0200

    buzzer works, changed buzz pattern

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from time import sleep
 
 buzzer = Buzzer(27)  # GPIO27, pin 13.
 
+# 2 Beeps and then turn off.
 buzzer.on()
-sleep(3)
+sleep(1)
+buzzer.off()
+sleep(1)
+buzzer.on()
+sleep(1)
 buzzer.off()
