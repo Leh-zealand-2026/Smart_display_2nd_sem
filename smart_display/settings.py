1c9f690b854029bf45af3f66b04bfb41793fa8cd
Leh001 <leh001@edu.zealand.dk>
2026-05-19 21:09:56 +0200

    created first html page

--- smart_display/settings.py
+++ smart_display/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Smart_display my apps
+    'webinterface',
 ]
 
 MIDDLEWARE = [
