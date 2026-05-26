ae9472e2321651d854170351cc887c623f4249e6
Leh001 <leh001@edu.zealand.dk>
2026-05-26 15:27:56 +0200

    error where homepage would not load if weather api failed to pull info, fixed by adding in temporary filler text

--- webinterface/views.py
+++ webinterface/views.py
@@ -4,9 +4,14 @@ from .API.open_meteo import get_weather
 from .API.google_calendar import get_upcoming_events
 
 # Create your views here.
-
+# filled with temp information because system did not work after failing to pull weather api 1 day, despite working earlier.
 def home(request):
-    weather = get_weather()
+    weather = {
+        "temperature": "-",
+        "weather_text": "Indlæser...",
+        "humidity": "-",
+        "wind_speed": "-"
+    }
 
     return render(request, "home.html", {
         "weather": weather
